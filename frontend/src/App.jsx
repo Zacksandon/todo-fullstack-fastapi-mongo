cce3c64dc339840ddc1bb73a4ba2cc6a80c16ae0
Zacksandon <200428542+Zacksandon@users.noreply.github.com>
2025-12-12 11:30:33 -0500

    Update API_URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const API_URL = 'http://127.0.0.1:8000'
+const API_URL = 'https://todo-fullstack-fastapi-mongo.onrender.com'
 
 function App() {
   const [todos, setTodos] = useState([])
