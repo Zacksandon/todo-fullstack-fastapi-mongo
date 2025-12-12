b3506aff9dbf741a360577ebf3e1de97b58aed76
Zacksandon <200428542+Zacksandon@users.noreply.github.com>
2025-12-12 11:39:23 -0500

    Refactor CORS middleware configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 app = FastAPI(title="Todo List Fullstack - MongoDB Atlas")
 
-# Lista de dominios permitidos (tu frontend)
 origins = [
     "https://todo-fullstack-fastapi-mongo.vercel.app",
     "http://localhost:5173",
@@ -16,7 +15,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,         # NO usar "*"
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -28,8 +27,9 @@ db = client.todoapp
 collection = db.todos
 
 from app.routers import todos
-app.include_router(todos.router, prefix="/api")  # <-- IMPORTANTE
+app.include_router(todos.router, prefix="/api")   # <-- SOLO AQUÍ SE COLOCA EL PREFIX
 
 @app.get("/")
 def home():
     return {"message": "Backend Todo List funcionando con MongoDB Atlas"}
+
